f901c33a62ac17ebbf31fbe2463ff9125ca20cde
Marcelo Schmitt <marcelo.schmitt@analog.com>
2023-05-25 17:31:05 -0300

    drivers: adc: Add support for AD7091R-2/-4/-8
    Add driver for AD7091R-2/-4/-8 devices which roughtly support the same
    operations as AD7091R5, but are interfaced through SPI.
    
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

---
+++ drivers/adc/ad7091r8/ad7091r8.c
@@ -0,0 +1,547 @@
+/***************************************************************************//**
+ *   @file   ad7091r8.c
+ *   @brief  Implementation of AD7091R-8 Driver
+ *   @author Marcelo Schmitt (marcelo.schmitt@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include <stdlib.h>
+#include <errno.h>
+
+#include "ad7091r8.h"
+#include "no_os_alloc.h"
+#include "no_os_delay.h"
+#include "no_os_util.h"
+
+
+// inspired by ad7091r, ad7091r5, ad7768-1,
+//TODO support IRQ
+
+
+/**
+ * Write to device.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_spi_reg_write(struct ad7091r8_dev *dev,
+			       uint8_t reg_addr,
+			       uint16_t reg_data)
+{
+	uint8_t buf[2];
+
+	if (!dev || !reg_data)
+		return -EINVAL;
+
+	/* Assumes controller host machine is little-endian */
+	buf[0] = AD7091R8_REG_WRITE(reg_addr);
+	buf[0] |= reg_data & 0x03;
+	buf[1] = reg_data & 0xFF;
+
+	return no_os_spi_write(dev->spi_desc, buf, 2);
+}
+
+/**
+ * Read from device.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_spi_reg_read(struct ad7091r8_dev *dev,
+			      uint8_t reg_addr,
+			      uint16_t *reg_data)
+{
+	uint8_t buf[2];
+	int32_t ret;
+
+	if (!dev || !reg_data)
+		return -EINVAL;
+
+	/* Assumes controller host machine is little-endian */
+	buf[0] = AD7091R8_REG_READ(reg_addr);
+	buf[1] = 0x00;
+
+	ret = no_os_spi_write_and_read(dev->spi_desc, buf, buf, 2);
+	if (ret < 0)
+		return ret;
+
+	/* Big-endian to little-endian */
+	*reg_data = (buf[0] << 8) | buf[1];
+
+	return ret;
+}
+
+/**
+ * SPI write to device using a mask.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param mask - The mask.
+ * @param data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_spi_write_mask(struct ad7091r8_dev *dev,
+				uint8_t reg_addr,
+				uint16_t mask,
+				uint16_t data)
+{
+	uint16_t reg_data;
+	int32_t ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	ret = ad7091r8_spi_reg_read(dev, reg_addr, &reg_data);
+	if (ret < 0)
+		return ret;
+
+	reg_data &= ~mask;
+	reg_data |= data;
+
+	return ad7091r8_spi_reg_write(dev, reg_addr, reg_data);
+}
+
+/**
+ * Set mode.
+ * @param dev - The device structure.
+ * @param mode - Converter mode.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_set_mode(struct ad7091r8_dev *dev, enum ad7091r8_mode mode)
+{
+	uint16_t data;
+
+	if (!dev)
+		return -EINVAL;
+
+	switch (mode) {
+	case AD7091R8_MODE_NORMAL:
+		data = 0;
+		break;
+	case AD7091R8_MODE_PW_DOWN:
+		data = REG_CONF_SLEEP_MODE(0x10)
+		break;
+	case AD7091R8_MODE_ALERT:
+		data = REG_CONF_GPO0_ALERT(1);
+		data &= ~REG_CONF_GPO0_BUSY(0);
+		break;
+	case AD7091R8_MODE_BUSY:
+		data = REG_CONF_GPO0_BUSY(1);
+		data &= ~REG_CONF_GPO0_ALERT(0);
+		break;
+	//case AD7091R8_MODE_SEQUENCER:
+	//	data = REG_CONF_AUTO(1); //TODO
+	//	break;
+	default:
+		return -EINVAL;
+	}
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+				       REG_CONF_MODE_MASK, data);
+}
+
+/**
+ * @brief Set device sleep mode.
+ *
+ * @param dev - The device structure.
+ * @param mode - The device sleep mode to set.
+ *
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_set_sleep_mode(struct ad7091r8_dev *dev,
+				enum ad7091r8_sleep_mode mode);
+{
+
+	if (!dev)
+		return -EINVAL;
+
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+				       REG_CONF_SLEEP_MODE_MASK,
+				       REG_CONF_SLEEP_MODE(mode));
+}
+
+/**
+ * Set output port value.
+ * @param dev - The device structure.
+ * @param port - Port number.
+ * @param value - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_set_port(struct ad7091r8_dev *dev, enum ad7091r8_port port,
+			  bool value)
+{
+	uint16_t mask;
+	uint16_t val;
+
+	if (!dev)
+		return -EINVAL;
+
+	switch (port) {
+	case AD7091R8_GPO0:
+		mask = REG_CONF_GPO0_MASK;
+		val = REG_CONF_GPO0(value);
+		break;
+	case AD7091R8_GPO1:
+		mask = REG_CONF_GPO1_MASK;
+		val = REG_CONF_GPO1(value);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF, mask, val);
+}
+
+/**
+ * Set GPO0 mode.
+ * @param dev - The device structure.
+ * @param mode - GPO0 new mode.
+ * @param is_cmos - 0: GPO0 is open drain
+ * 		  - 1: GPO0 is CMOS.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_set_gpo0_mode(struct ad7091r8_dev *dev,
+			       enum ad7091r8_gpo0_mode mode, bool is_cmos)
+{
+	uint16_t value;
+
+	if (!dev)
+		return -EINVAL;
+
+	switch (mode) {
+	case AD7091R8_GPO0_ENABLED:
+		value = 0;
+		break;
+	case AD7091R8_GPO0_ALERT:
+		value = REG_CONF_GPO0_ALERT(1);
+		break;
+	case AD7091R8_GPO0_BUSY:
+		value = REG_CONF_GPO0_BUSY(1) | REG_CONF_GPO0_ALERT(1);
+		break;
+	default:
+		return -EINVAL;
+		break;
+	}
+	value |= REG_CONF_GPO0_DRIVE_TYPE(is_cmos);
+
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+				       REG_CONF_GPO0_MODE_MASK, value);
+}
+
+/**
+ * Set high limit, low limit, hysteresis.
+ * Device accepts 9 bits provided by the user and sets them as the MSBs.
+ * The 3 LSBs of the internal 12-bit registers are set either to 000 or 111.
+ * Round user input according to each case.
+ * @param dev - The device structure.
+ * @param limit - Limit.
+ * @param channel - Channel.
+ * @param value - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_set_limit(struct ad7091r8_dev *dev,
+			   enum ad7091r8_limit limit,
+			   uint8_t channel,
+			   uint16_t value)
+{
+	uint16_t reg;
+
+	if (!dev)
+		return -EINVAL;
+
+	switch (limit) {
+	case AD7091R8_LOW_LIMIT:
+		reg = AD7091R8_REG_CH_LOW_LIMIT(channel);
+		value += (value & 0x04) ? 8 : 0;
+		break;
+	case AD7091R8_HIGH_LIMIT:
+		reg = AD7091R8_REG_CH_HIGH_LIMIT(channel);
+		value -= (value & 0x04) ? 8 : 0;
+		break;
+	case AD7091R8_HYSTERESIS:
+		reg = AD7091R8_REG_CH_HYSTERESIS(channel);
+		value += (value & 0x04) ? 8 : 0;
+		break;
+	default:
+		return -EINVAL;
+	}
+	value = value >> 3;
+
+	return ad7091r8_spi_reg_write(dev, reg, REG_RESULT_CONV_DATA(value));
+}
+
+/**
+ * Get alert.
+ * @param dev - The device structure.
+ * @param channel - Channel.
+ * @param alert - Alert type.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_get_alert(struct ad7091r8_dev *dev, uint8_t channel,
+			   enum ad7091r8_alert_type *alert)
+{
+	int32_t ret;
+	uint16_t data;
+
+	if (!dev || !alert)
+		return -EINVAL;
+
+	if (channel >= AD7091R8_NUM_CHANNELS)
+		return -EINVAL;
+
+	ret = ad7091r8_spi_reg_read(dev, AD7091R8_REG_ALERT, &data);
+	if (ret)
+		return ret;
+
+	*alert = REG_ALERT_MASK(data, channel);
+
+	return 0;
+}
+
+/**
+ * Get high limit, low limit, hysteresis.
+ * The 3 LSBs of the internal 12-bit registers are set either to 000 or 111.
+ * Adjust limit data to reflect actual limit in use by the device.
+ * @param dev - The device structure.
+ * @param limit - Limit.
+ * @param channel - Channel.
+ * @param value - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_get_limit(struct ad7091r8_dev *dev,
+			   enum ad7091r8_limit limit,
+			   uint8_t channel,
+			   uint16_t *value)
+{
+	int32_t ret;
+	uint16_t reg, data;
+
+	if (!dev || !value)
+		return -EINVAL;
+
+	switch (limit) {
+	case AD7091R8_LOW_LIMIT:
+		reg = AD7091R8_REG_CH_LOW_LIMIT(channel);
+		break;
+	case AD7091R8_HIGH_LIMIT:
+		reg = AD7091R8_REG_CH_HIGH_LIMIT(channel);
+		*value = 0x07;
+		break;
+	case AD7091R8_HYSTERESIS:
+		reg = AD7091R8_REG_CH_HYSTERESIS(channel);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	ret = ad7091r8_spi_reg_read(dev, reg, &data);
+	if (ret)
+		return ret;
+
+	*value |= REG_CHAN_LIMIT_DATA(data) << 3;
+
+	return 0;
+}
+
+/***************************************************************************//**
+ * @brief Initializes the communication peripheral and the initial Values for
+ *        AD7092R-8 Board.
+ *
+ * @param device     - The device structure.
+ * @param init_param - The structure that contains the device initial
+ * 		       parameters.
+ *
+ * @return ret - The result of the initialization procedure.
+ *               Example: -1 - SPI peripheral was not initialized or the
+ *                             device is not present.
+ *                         0 - SPI peripheral was initialized and the
+ *                             device is present.
+*******************************************************************************/
+int8_t ad7091r8_init(struct ad7091r8_dev **device,
+		     struct ad7091r8_init_param init_param)
+{
+	struct ad7091r8_dev *dev;
+	uint8_t status = 0;
+	//uint8_t tmp_val = 0xFF;
+
+	if (!device || !init_param)
+		return -EINVAL;
+
+	dev = (struct ad7091r8_dev *)no_os_malloc(sizeof(*dev));
+	if (!dev)
+		return -ENOMEM;
+
+	ret = no_os_spi_init(&dev->spi_desc, &init_param.spi_init);
+	if (ret < 0)
+		return ret;
+
+	//TODO apply config
+
+	dev->gpio_reset = NULL;
+	dev->gpio_alert = NULL;
+
+	no_os_gpio_get_optional(&dev->gpio_reset, init_param->gpio_reset);
+	if (dev->gpio_reset != NULL)
+		ad7091r8_reset(dev, false);
+
+	no_os_gpio_get_optional(&dev->gpio_alert, init_param->gpio_alert);
+
+	/* Use normal mode by default */
+	ret = ad7091r8_set_mode(dev, AD7091R8_CONFIG_MODE_NORMAL);
+	if (ret < 0)
+		goto error_i2c;
+
+	*device = dev;
+
+	return ret;
+}
+
+/**
+ * @brief Initiate a software reset or hardware reset through the RESET pin.
+ * @param dev - ad7091r8_dev device handler.
+ * @param is_software - true: Software reset
+ * 		      - false: hardware reset
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_reset(struct ad7091r8_dev *dev, bool is_software)
+{
+	int32_t ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	if (is_software) {
+		/* Bit is cleared automatically after reset */
+		return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+					       REG_CONF_RESET_MASK,
+					       REG_CONF_RESET(1));
+	} else {
+		/* reset pulse delay upon power up, at least 50 ns */
+		no_os_udelay(5);
+		ret = no_os_gpio_set_value(dev->gpio_resetn, NO_OS_GPIO_LOW);
+		if (ret < 0)
+			return ret;
+
+		/* reset pulse width, at least 10 ns */
+		no_os_udelay(1);
+		return no_os_gpio_set_value(dev->gpio_resetn, NO_OS_GPIO_HIGH);
+	}
+}
+
+/***************************************************************************//**
+ * @brief Free the resources allocated by ad7091r8_init().
+ *
+ * @param dev - The device structure.
+ *
+ * @return ret - The result of the remove procedure.
+*******************************************************************************/
+int32_t ad7091r8_remove(struct ad7091r8_dev *dev)
+{
+	int32_t ret;
+
+	ret = no_os_spi_remove(dev->spi_desc);
+
+	no_os_free(dev);
+
+	return ret;
+}
+
+/**
+ * Set device channel.
+ * @param dev - The device structure.
+ * @param channel - Channel.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t ad7091r8_set_channel(struct ad7091r8_dev *dev, uint8_t channel)
+{
+	uint16_t foo;
+	int32_t ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	if (channel >= AD7091R8_NUM_CHANNELS)
+		return -EINVAL;
+
+	ret = ad7091r8_spi_reg_write(dev, AD7091R8_REG_CHANNEL,
+				     NO_OS_BIT(channel));
+	if (ret)
+		return ret;
+
+	/* There is a latency of one conversion before the channel conversion
+	 * sequence is updated */
+	return ad7091r8_spi_reg_read(dev, AD7091R8_REG_RESULT, &foo);
+}
+
+/***************************************************************************//**
+ * @brief Initiates one conversion and reads back the result. During this
+ *        process the device runs in normal mode and operates without the busy
+ *        indicator.
+ *
+ * @param dev - The device structure.
+ *
+ * @return conversionResult - 12bit conversion result.
+*******************************************************************************/
+/**
+ * Read one sample.
+ * @param dev - The device structure.
+ * @param channel - Channel.
+ * @param read_val - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+uint16_t ad7091r8_read_one(struct ad7091r8_dev *dev, uint8_t chan,
+			   uint16_t *read_val)
+{
+	if (!dev || !read_val)
+		return -EINVAL;
+
+	if (channel >= AD7091R8_NUM_CHANNELS) //TODO adap for -2/-4
+		return -EINVAL;
+
+	ret = ad7091r8_set_channel(dev, channel);
+	if (ret)
+		return ret;
+
+	ret = ad7091r8_spi_reg_read(dev, AD7091R8_REG_RESULT, &val);
+	if (ret)
+		return ret;
+
+	if (REG_RESULT_CH_ID(val) != channel)
+		return -1;
+
+	*read_val = REG_RESULT_CONV_DATA(val);
+
+	return 0;
+}
---
+++ drivers/adc/ad7091r8/ad7091r8.h
@@ -0,0 +1,283 @@
+/***************************************************************************//**
+ *   @file   ad7091r8.h
+ *   @brief  Implementation of AD7091R-8 Driver
+ *   @author Marcelo Schmitt (marcelo.schmitt@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef __AD7091R8_H__
+#define __AD7091R8_H__
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+#define AD7091R2_NUM_CHANNELS				2
+#define AD7091R4_NUM_CHANNELS				4
+#define AD7091R8_NUM_CHANNELS				8
+#define AD7091R8_BITS					12
+
+#define AD7091R8_CONV_MASK		NO_OS_GENMASK(AD7091R8_BITS - 1, 0)
+//#define AD7091R8_CONV_DATA_MASK		NO_OS_GENMASK(AD7091R8_BITS - 1, 0)
+//#define AD7091R8_CONV_ALERT_MASK	NO_OS_BIT(12)
+//#define AD7091R8_CONV_CHAN_MASK		NO_OS_GENMASK(15, 13)
+#define AD7091R8_CHAN_LIMIT_MSK		NO_OS_GENMASK(8, 0)
+
+/* AD7091r8 registers */
+#define AD7091R8_REG_RESULT		0x00
+#define AD7091R8_REG_CHANNEL		0x01
+#define AD7091R8_REG_CONF		0x02
+#define AD7091R8_REG_ALERT		0x03
+#define AD7091R8_REG_CH_LOW_LIMIT(ch)	((ch) * 3 + 4)
+#define AD7091R8_REG_CH_HIGH_LIMIT(ch)	((ch) * 3 + 5)
+#define AD7091R8_REG_CH_HYSTERESIS(ch)	((ch) * 3 + 6)
+
+/* AD7091R8_REG_RESULT */
+//#define REG_RESULT_CH_ID(x)		no_os_field_get(AD7091R8_CONV_CHAN_MASK, (x))
+//#define REG_RESULT_CONV_DATA(x)		((x) & AD7091R8_CONV_MASK)
+#define REG_RESULT_CH_ID(x)		(((x) >> 12) & 0x7)
+#define REG_RESULT_CONV_DATA(x)		((x) & AD7091R8_CONV_MASK)
+
+/* AD7091R8_REG_CONF */
+#define REG_CONF_SLEEP_MODE_MASK	(NO_OS_BIT(1) | NO_OS_BIT(0))
+#define REG_CONF_SLEEP_MODE(x)		((x & 0x03) << 0)
+
+#define REG_CONF_GPO1_MASK		NO_OS_BIT(2)
+#define REG_CONF_GPO1(x)		((x & 0x01) << 2)
+
+#define REG_CONF_GPO0_MASK		NO_OS_BIT(3)
+#define REG_CONF_GPO0(x)		((x & 0x01) << 3)
+
+#define REG_CONF_GPO0_MODE_MASK		(NO_OS_BIT(6) | NO_OS_BIT(5) | NO_OS_BIT(4))
+#define REG_CONF_GPO0_ALERT(x)		((x & 0x01) << 4)
+#define REG_CONF_GPO0_BUSY(x)		((x & 0x01) << 5)
+#define REG_CONF_GPO0_DRIVE_TYPE(x)	((x & 0x01) << 6)
+
+#define REG_CONF_ALERT_STICKY_MASK	NO_OS_BIT(7)
+#define REG_CONF_ALERT_STICKY(x)	((x & 0x01) << 7)
+
+#define REG_CONF_RESET_MASK		NO_OS_BIT(9)
+#define REG_CONF_RESET(x)		((x & 0x01) << 9)
+
+/* AD7091R8_REG_ALERT */
+#define REG_ALERT_MASK(x, ch)		(x >> (ch * 2))
+
+/* AD7091R8_REG_CHAN_LIMIT */
+#define REG_CHAN_LIMIT_DATA(x)		((x) & AD7091R8_CHAN_LIMIT_MSK)
+
+/* AD7091R8 read/write protocol most significant byte */
+#define AD7091R8_REG_READ(x)	( ((x) << 3) | (1 << 2) )	// Read from register x
+#define AD7091R8_REG_WRITE(x)	( ((x) << 3) & (~(1 << 2)) )	// Write to register x
+
+/*****************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+enum ad7091r8_device_id {
+	AD7091R2,
+	AD7091R4,
+	AD7091R8,
+};
+
+/**
+ * @enum ad7091r8_mode
+ * @brief Converter supported modes
+ */
+enum ad7091r8_mode {
+	/* Normal mode, conversion initiates on the falling edge of CONVST */
+	AD7091R8_MODE_NORMAL = 0,
+	/* Power down mode, set by writing to configuration register */
+	AD7091R8_MODE_PW_DOWN,
+	/* Alert mode, set by writing to configuration register */
+	AD7091R8_MODE_ALERT,
+	/* Busy mode, set by writing to configuration register */
+	AD7091R8_MODE_BUSY,
+	/* Channel sequencer mode, set by writing to channel register */
+	AD7091R8_MODE_SEQUENCER,
+}
+
+/**
+ * @enum ad7091r8_sleep_mode
+ * @brief Converter supported sleep modes
+ */
+enum ad7091r8_sleep_mode {
+	/** Default operation:
+	 * Sleep mode Off, Internal reference Off */
+	AD7091R8_SLEEP_MODE_0,
+	/** Sleep mode Off, Internal reference On */
+	AD7091R8_SLEEP_MODE_1,
+	/** Sleep mode On, Internal reference Off */
+	AD7091R8_SLEEP_MODE_2,
+	/** Sleep mode On, Internal reference On */
+	AD7091R8_SLEEP_MODE_3,
+};
+
+/**
+ * @enum ad7091r8_port
+ * @brief Converter general purpose outputs
+ */
+enum ad7091r8_port {
+	/** GPO0 */
+	AD7091R8_GPO0,
+	/** GPO1 */
+	AD7091R8_GPO1,
+};
+
+/**
+ * @enum ad7091r8_gpo0_mode
+ * @brief Port 0 configuration
+ */
+enum ad7091r8_gpo0_mode {
+	/** GPO0 is output port */
+	AD7091R8_GPO0_ENABLED,
+	/** GPO0 is Alert indicator */
+	AD7091R8_GPO0_ALERT,
+	/** GPO0 is busy indicator, device is converting */
+	AD7091R8_GPO0_BUSY,
+};
+
+/**
+ * @enum ad7091r8_limit
+ * @brief Limit type
+ */
+enum ad7091r8_limit {
+	/** Low limit */
+	AD7091R8_LOW_LIMIT,
+	/** High limit */
+	AD7091R8_HIGH_LIMIT,
+	/** Hysteresis */
+	AD7091R8_HYSTERESIS,
+};
+
+/**
+ * @enum ad7091r8_alert_type
+ * @brief Alert status
+ */
+enum ad7091r8_alert_type {
+	/** No alert */
+	AD7091R8_NO_ALERT,
+	/** High alert */
+	AD7091R8_HIGH_ALERT,
+	/** Low alert */
+	AD7091R8_LOW_ALERT,
+};
+
+struct ad7091r8_config {
+	// attribs
+	enum ad7091r8_mode mode;
+};
+
+struct ad7091r8_dev {
+	/** SPI descriptor **/
+	//spi_desc *spi_desc;
+	struct no_os_spi_desc *spi_desc;
+	/** RESET GPIO handler. */
+	struct no_os_gpio_desc	*gpio_reset;
+	/** ALERT GPIO handler. */
+	struct no_os_gpio_desc	*gpio_alert;
+	enum ad7091r8_device_id device_di;
+	/* Configuration register settings */
+	struct ad7091r8_config config;
+	//uint8_t data_buffer[8];
+	enum ad7091r8_mode mode;
+}
+
+struct ad7091r8_init_param {
+	/* SPI initialization parameters */
+	struct no_os_spi_init_param *spi_init;
+	/* GPIO initialization parameters */
+	//struct gpio_init_param *gpio1;
+	struct no_os_gpio_init_param *gpio_reset;
+	/* Alert GPIO initialization parameters */
+	struct no_os_gpio_init_param *gpio_alert;
+	/* AD7091R specific device identifier */
+	enum ad7091r8_device_id device_id;
+	/* Configuration register settings */
+	struct ad7091r8_config config;
+}
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+/* Initialize the device. */
+int8_t ad7091r8_init(struct ad7091r8_dev **device,
+		     struct ad7091r8_init_param init_param);
+
+/* Remove the device and release resources. */
+int32_t ad7091r8_remove(struct ad7091r8_dev *dev);
+
+/* Set device mode */
+int32_t ad7091r8_set_mode(struct ad7091r8_dev *dev,
+			  enum ad7091r8_mode mode);
+
+/* Set device sleep mode */
+int32_t ad7091r8_set_sleep_mode(struct ad7091r8_dev *dev,
+				enum ad7091r8_sleep_mode mode);
+
+/* Set device set port value */
+int32_t ad7091r8_set_port(struct ad7091r8_dev *dev,
+			  enum ad7091r8_port port,
+			  bool value);
+
+/* Set device set GPO0 mode */
+int32_t ad7091r8_set_gpo0_mode(struct ad7091r8_dev *dev,
+			       enum ad7091r8_gpo0_mode mode,
+			       bool is_cmos);
+
+/* Set high limit, low limit, hysteresis. */
+int32_t ad7091r8_set_limit(struct ad7091r8_dev *dev,
+			   enum ad7091r8_limit limit,
+			   uint8_t channel,
+			   uint16_t value);
+
+/* Get alert. */
+int32_t ad7091r8_get_alert(struct ad7091r8_dev *dev,
+			   uint8_t channel,
+			   enum ad7091r8_alert_type *alert);
+
+/* Get high limit, low limit, hysteresis. */
+int32_t ad7091r8_get_limit(struct ad7091r8_dev *dev,
+			   enum ad7091r8_limit limit,
+			   uint8_t channel,
+			   uint16_t *value);
+
+/* Select device channel. */
+int32_t ad7091r8_set_channel(struct ad7091r8_dev *dev,
+			     uint8_t channel)
+
+/* Read one sample. */
+uint16_t ad7091r8_read_one(struct ad7091r8_dev *dev,
+			   uint8_t chan,
+			   uint16_t *read_val)
+
+#endif // __AD7091R8_H__
