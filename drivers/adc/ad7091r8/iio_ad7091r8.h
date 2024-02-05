cd8d6f446fe6ebc83d9db5e76233919db50382f1
Marcelo Schmitt <marcelo.schmitt@analog.com>
2024-02-05 17:51:04 -0300

    drivers/adc/ad7091r8/iio_ad7091r8.h: Add comments for doxygen
    Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

--- drivers/adc/ad7091r8/iio_ad7091r8.h
+++ drivers/adc/ad7091r8/iio_ad7091r8.h
@@ -61,11 +61,17 @@
 /******************************************************************************/
 extern struct iio_trigger ad7091r8_iio_timer_trig_desc;
 
+/** @struct ad7091r8_iio_dev
+ *  @brief AD7091r8 IIO device descriptor structure
+ */
 struct ad7091r8_iio_dev {
 	struct ad7091r8_dev *ad7091r8_dev;
 	struct iio_device *iio_dev;
 };
 
+/** @struct ad7091r8_iio_dev_init_param
+ *  @brief AD7091r8 IIO device initial parameters structure
+ */
 struct ad7091r8_iio_dev_init_param {
 	struct ad7091r8_init_param *ad7091r8_dev_init;
 };
@@ -73,9 +79,11 @@ struct ad7091r8_iio_dev_init_param {
 /******************************************************************************/
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
+/*! Function to be called to initialize a AD7091R-2/-4/-8 device. */
 int ad7091r8_iio_init(struct ad7091r8_iio_dev **iio_dev,
 		      struct ad7091r8_iio_dev_init_param *init_param);
 
+/*! Function to be called to remove a AD7091R-2/-4/-8 device. */
 int ad7091r8_iio_remove(struct ad7091r8_iio_dev *desc);
 
 #endif /** IIO_AD7091R8_H */
